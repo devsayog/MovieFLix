1e29998307bb56884384647d75966ef38cd20539
devsayog <devsayog@gmail.com>
2022-07-28 18:05:03 +0545

    initalize project

---
+++ src/components/hero.tsx
@@ -0,0 +1,5 @@
+function Hero() {
+  return <h1 className="text-6xl">Hero</h1>
+}
+
+export default Hero
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import eslint from 'vite-plugin-eslint'
+
+export default defineConfig({
+  plugins: [react(), tsConfigPaths(), eslint()],
+})
