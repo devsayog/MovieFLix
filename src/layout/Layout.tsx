751a233b7766b7a12fd96af40e46f79d5d868294
devsayog <devsayog@gmail.com>
2022-08-01 19:52:32 +0545

    make main element min-h-full

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -5,7 +5,7 @@ function Layout() {
   return (
     <div className="flex bg-slate-200 dark:bg-gray-900 dark:text-gray-200">
       <AppbarandSidebar />
-      <main className="flex-1 mt-20 p-3">
+      <main className="flex-1 mt-20 p-3 min-h-screen md:ml-60">
         <AppRoutes />
       </main>
     </div>
