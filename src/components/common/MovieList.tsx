6655ccac587bb9165ce6ee624a7e7e2a040ce424
devsayog <devsayog@gmail.com>
2022-08-01 09:53:15 +0545

    refactor MovieList component to accept number of movies as props and display accordingly

--- src/components/common/MovieList.tsx
+++ src/components/common/MovieList.tsx
@@ -4,16 +4,23 @@ import Movie from './Movie'
 
 interface MovieListProps {
   movies: MoviesType
-  exclude: number
+  exclude?: number
+  numberOfMovies?: number
 }
-function MovieList({ movies, exclude }: MovieListProps) {
+function MovieList({ movies, exclude, numberOfMovies }: MovieListProps) {
+  const numMovies = numberOfMovies === 0 ? undefined : numberOfMovies
   return (
     <div className="mt-3 flex gap-4 justify-center flex-wrap overflow-hidden">
-      {movies.results.slice(exclude).map((movie) => (
+      {movies.results.slice(exclude, numMovies).map((movie) => (
         <Movie key={generateUniqueId()} movie={movie} />
       ))}
     </div>
   )
+}
+
+MovieList.defaultProps = {
+  numberOfMovies: 0,
+  exclude: 0,
 }
 
 export default MovieList
