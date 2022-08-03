898841ac58a81271d495bb5158f34c54bb2b16fe
devsayog <devsayog@gmail.com>
2022-08-03 13:55:25 +0545

    code splitting

--- src/views/Routes.tsx
+++ src/views/Routes.tsx
@@ -1,19 +1,24 @@
+import { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import ActorDescription from './ActorDescription'
-import MovieDescription from './MovieDescription'
-import Movies from './Movies'
+import Spinner from '@components/common/Spinner'
+
+const Movies = lazy(() => import('./Movies'))
+const ActorDescription = lazy(() => import('./ActorDescription'))
+const MovieDescription = lazy(() => import('./MovieDescription'))
 
 function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/">
-        <Route index element={<Movies />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="movies/:genreOrCat" element={<Movies />} />
-        <Route path="movie/:id" element={<MovieDescription />} />
-        <Route path="actors/:id" element={<ActorDescription />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Movies />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:genreOrCat" element={<Movies />} />
+          <Route path="movie/:id" element={<MovieDescription />} />
+          <Route path="actors/:id" element={<ActorDescription />} />
+        </Route>
+      </Routes>
+    </Suspense>
   )
 }
 
