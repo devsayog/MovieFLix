6f6f61999d85cb8302c0ff2e9caf0ceff47fdaec
devsayog <devsayog@gmail.com>
2022-08-03 11:10:44 +0545

    remove padding on xs screen, add more padding according to screen sizes in main element

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -5,7 +5,7 @@ function Layout() {
   return (
     <div className="flex bg-slate-200 dark:bg-gray-900 dark:text-gray-200">
       <AppbarandSidebar />
-      <main className="flex-1 mt-20 p-3 min-h-screen md:ml-60">
+      <main className="flex-1 mt-20 sm:p-3 md:p-4 lg:p-5 min-h-screen md:ml-60">
         <AppRoutes />
       </main>
     </div>
